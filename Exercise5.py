491e932d5f882661ddf1fd4d0f646bf1615bc691
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:45:03 +1000

    Add Exercise 5 - Task Information Form

---
+++ Exercise5.py
@@ -0,0 +1,18 @@
+#Program: Task Tracker - Task Information Form
+#Author: Saad
+#Description: This program collects task information and displays them in a formatted manner
+
+task_id = input("Enter Task ID: ")
+task_name = input("Enter Task Name: ")
+department = input("Enter Department: ")
+estimated_hours = input("Enter Estimated Hours: ")
+
+print ()
+print ("=" * 30)
+print ("Task Information Form")
+print ("=" * 30)
+print ("Task ID:", task_id)
+print ("Task Name:", task_name)
+print ("Department:", department)
+print ("Estimated Hours:", estimated_hours)
+print ()
