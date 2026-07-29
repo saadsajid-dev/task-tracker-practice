427481cbd3e885d4f19ab5c9bea81266bcdaf067
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:23:03 +1000

    Add Exercise 1 - Welcome Message

---
+++ Exercise1.py
@@ -0,0 +1,14 @@
+#Program: Task Tracker - Welcome Message
+#Author: Saad
+#Description: This program displays a welcome banner, student name, and today's date
+
+print ("=" * 29)
+print ("Task Tracker")
+print ("=" * 29)
+print ("Welcome to the Task Tracker program!")
+
+student_name = "Saad Sajid"
+today_date = "2026-07-29"
+
+print ("Student Name:", student_name)
+print ("Today's Date:", today_date)
