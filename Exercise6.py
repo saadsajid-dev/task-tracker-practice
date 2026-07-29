c9d9bd245eb07383b8ec210a76ef466462188833
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:50:42 +1000

    Add Exercise 6 - Daily Work Log

---
+++ Exercise6.py
@@ -0,0 +1,15 @@
+#Program: Task Tracker - Daily Work Log
+#Author: Saad
+#Description: This program collects daily work information and displays it
+
+employee_name = "Saad Sajid"
+task_completed = 9
+working_hours = 8.5
+attendance_marked = True
+
+print ()
+print ("Employee Name:", employee_name)
+print ("Task Completed:", task_completed)
+print ("Working Hours:", working_hours)
+print ("Attendance Marked:", attendance_marked)
+print ()
