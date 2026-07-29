0b18efefbf515304079b11be800b1bdcd5425d7e
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:54:53 +1000

    Add Exercise 7 - Project Registration

---
+++ Exercise7.py
@@ -0,0 +1,13 @@
+#Program: Task Tracker - Project Registration
+#Author: Saad
+#Description: This program collects project details from the user and displays them
+
+project_name = input("Enter Project Name: ")
+team_leader = input("Enter Team Leader Name: ")
+number_of_members = input("Enter Number of Team Members: ")
+
+print ()
+print ("Project Name:", project_name)
+print ("Team Leader:", team_leader)
+print ("Team Members:", number_of_members)
+print ()
