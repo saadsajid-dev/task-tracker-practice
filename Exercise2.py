35688bb8d13b1dc7526f1bec3697f9f51101fc5e
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:29:27 +1000

    Add Exercise 2 - Add a Task

---
+++ Exercise2.py
@@ -0,0 +1,11 @@
+#Program: Task Tracker - Create a Task
+#Author: Saad
+#Description: This program stores a task with its details and displays them
+
+task_id = 101
+task_name = "Design Login Page"
+priority = "High"
+
+print ("Task ID:", task_id)
+print ("Task Name:", task_name)
+print ("Priority:", priority)
