86e081b9dd162fc893d19d89414f97abe271bb45
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:39:08 +1000

    Add Exercise 4 - Employee Task Assignment

---
+++ Exercise4.py
@@ -0,0 +1,9 @@
+#Program: Task Tracker - Employee Task Assignment
+#Author: Saad
+#Description: This program collects employee name and assigned task, then displays them
+
+employee_name = input("Enter Employee Name: ")
+assigned_task = input("Enter Assigned Task: ")
+
+print ("Employee Name:", employee_name)
+print ("Assigned Task:", assigned_task)
