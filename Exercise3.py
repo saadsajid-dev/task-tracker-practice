7801f891382419553529fbd37285d2db5e62d85b
Saad Sajid <saadsajid47@icloud.com>
2026-07-29 14:34:29 +1000

    Add Exercise 3 - Display Data Types

---
+++ Exercise3.py
@@ -0,0 +1,20 @@
+#Program: Task Tracker - Display Data Types
+#Author: Saad
+#Description: This program creates variables of different data types and displays their values + types
+
+task_name = "Design Login Page"  # String
+task_id = 201
+estimated_hours = 6.5
+task_completed = False
+
+print ("Task Name:", task_name)
+print (type(task_name))
+
+print ("Task ID:", task_id)
+print (type(task_id))
+
+print ("Estimated Hours:", estimated_hours)
+print (type(estimated_hours))
+
+print ("Task Completed:", task_completed)
+print (type(task_completed))
